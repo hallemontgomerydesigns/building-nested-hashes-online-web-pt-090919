656dc4656ac35b8b5fce5e0f2bbaf44d851846ab
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-01-30 16:11:23 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,22 +1,32 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
+      :patriarch => { 
+        :name => "Lord Montague",
+        :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
         #your key/value pairs here
         },
       :hero => {
+        :name => "Romeo",
+        :age => "15"
         #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Capulet",
+        :age =>
         #your key/value pairs here
         },
       :heroine => {
